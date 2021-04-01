712cbe013cac828e86c9871d1109c89932c5121f
Abdullah Veliyev <abdullahveliyev2005@gmail.com>
2021-04-01 22:44:33 +0400

    completed section 1

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,24 @@
       </div>
 
       <div class="container2">
-
+        <h1>Little about me</h1>
+        <p>
+          My name is Abdullah. I was born in Baku,
+          Azerbaijan in 2005. I started my school life
+          in 2011. I am studying in 10th grade. I
+          started to be interested in programming in
+          March 2020.My motivation to start
+          programming was to be beneficial to people
+          by doing my dream projects, which is the
+          motivation for me to continue programming.
+        </p>
+        <div class="social-buttons-container">
+          <a target="_blank" href="https://github.com/Abdullah-V"><button class="social-button"><i class="fab fa-github"></i></button></a>
+          <a target="_blank" href="https://twitter.com/Abdullah_V_2005"><button class="social-button"><i class="fab fa-twitter"></i></button></a>
+          <a target="_blank" href="https://t.me/Abdullah_V_2005"><button class="social-button"><i class="fab fa-telegram-plane"></i></button></a>
+          <a target="_blank" href="mailto:abdullahveliyev2005@gmail.com"><button class="social-button"><i class="fas fa-envelope"></i></button></a>
+          <a target="_blank" href="https://discord.com/users/715280927575507034"><button class="social-button"><i class="fab fa-discord"></i></button></a>
+        </div>
       </div>
     </div>
 
@@ -47,7 +64,11 @@ export default {}
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  background: yellow;
+  background: white;
+}
+
+.section1 .container1 i {
+  color: var(--primary);
 }
 
 .image-container {
@@ -76,11 +97,45 @@ p {
   background: var(--primary);
   color: white;
   font-weight: normal;
+  font-size: 18px;
   transition: 300ms all;
+  padding: 12px 40px;
+  border-radius: 3px;
 }
 
 .resume-button:hover {
-  background: var(--secondary);
+  transform: scale(1.08);
+}
+
+.section1 .container2 {
+  width: 550px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.social-buttons-container {
+  width: 80%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.social-button {
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background: var(--primary);
+  color: white;
+  font-size: 22px;
+  transition: 300ms all;
+}
+
+.social-button:hover {
+  transform: scale(1.08);
 }
 
 </style>
